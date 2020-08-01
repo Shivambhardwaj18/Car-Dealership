5ae340bcf36e031910c9f6e3c5c77809804c118d
SHIVAM BHARDWAJ <SHIVAM BHARDWAJ@DESKTOP-SVSIR63.mshome.net>
2020-08-01 13:19:45 +0530

    First Commit Uploading All files.

---
+++ car_dealership/src/car_dealership/Employee.java
@@ -0,0 +1,21 @@
+package car_dealership;
+
+public class Employee {
+	public void handleCustomer(Customer cust,boolean finance, Vehicle vehicle) {
+		if(finance == true) {
+			double loanAmount = vehicle.getPrice() - cust.getCashOnHand();
+			runCreditHistory(cust,loanAmount);
+		}else if(vehicle.getPrice() <= cust.getCashOnHand()) {
+			processTransaction(cust,vehicle);
+		}else 
+			System.out.println("customer will need more money to purchase vehicle: "+vehicle);
+	}
+	public void runCreditHistory(Customer cust,double loanAmount) {
+		System.out.println("Ran credit history for loanAmount");
+		System.out.println("Customer has been approved to purchase the vehicle");
+	}
+	public void processTransaction(Customer cust,Vehicle vehicle) {
+		System.out.println("Customer has purchasedd the vehicle: "+vehicle+"for the price "+vehicle.getPrice());
+	}
+}
+
